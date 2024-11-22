80c04fc6f4a6f96b359e6b6e6a4eaac92c9d7696
TheGoderGuy <julian.preuss@pflege.de>
2024-11-22 15:01:47 +0100

    feat: Add support for edited events to run the action on every PR edit (#118)
    Co-authored-by: Cristopher <32661241+Namchee@users.noreply.github.com>

--- internal/constants/event.go
+++ internal/constants/event.go
@@ -2,5 +2,5 @@ package constants
 
 var (
 	// Events stores a list of pull request sub-events to be processed
-	Events = []string{"opened", "reopened", "ready_for_review", "synchronize", "unlocked"}
+	Events = []string{"opened", "reopened", "ready_for_review", "synchronize", "unlocked", "edited"}
 )
